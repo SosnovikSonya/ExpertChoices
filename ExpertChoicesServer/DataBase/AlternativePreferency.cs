18a638828c389df39f437561d9ff5b1792d67f84
MikhailSasnavik <Mikhail_Sasnavik@epam.com>
2019-05-27 00:17:42 +0300

    Last change

--- ExpertChoicesServer/DataBase/AlternativePreferency.cs
+++ ExpertChoicesServer/DataBase/AlternativePreferency.cs
@@ -7,7 +7,7 @@ namespace ExpertChoicesServer.DataBase
 {
     public class AlternativePreferency
     {
-        public int IdAlternativesPreferency { get; set; }
+        public int IdAlternativePreferency { get; set; }
         public int IdProblem { get; set; }
         public int IdAlternative { get; set; }
         public float Value { get; set; }
